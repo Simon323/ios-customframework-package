570fc32c6528a1959e7609da17c6f66b9e5d7c16
Szymon Miękus <miekusoft@gmail.com>
2023-06-26 22:50:21 +0200

    Initial commit

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Miekusoft on 26/06/2023.
+//
+
+import Foundation
+
+public struct SDK {
+    public static func doSomeWork() {
+        print("Doing some work")
+    }
+}
